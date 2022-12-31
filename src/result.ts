d3654ce60d1701b718e9963c864f5729bb433e5e
Alan Johnson <nalanj@fastmail.com>
2022-12-31 11:50:47 -0500

    Update result to be more like Rust

--- src/result.ts
+++ src/result.ts
@@ -1,9 +1,21 @@
-export type Result<T, E = Error> = T | E;
+export type ResultOk<T> = { ok: true; value: T };
+export type ResultErr<E> = { ok: false; error: E };
+export type Result<T, E extends Error = Error> = ResultOk<T> | ResultErr<E>;
 
-export function ok<T>(r: Result<T>): r is T {
-  return !(r instanceof Error);
+export function Ok<T>(value: T): ResultOk<T> {
+  return { ok: true, value };
 }
 
-export function notOk<E>(r: Result<unknown, E>): r is E {
-  return r instanceof Error;
+export function Err<E>(error: E): ResultErr<E> {
+  return { ok: false, error };
+}
+
+export function isOk<T>(r: Result<T, Error>): r is ResultOk<T> {
+  return r.ok;
+}
+
+export function isErr<E extends Error>(
+  r: Result<unknown, E>
+): r is ResultErr<E> {
+  return !r.ok;
 }
