5c8232a72f4802bd8bbf759175ed740b06703b5d
Alan Johnson <nalanj@fastmail.com>
2022-12-31 12:01:52 -0500

    Improve result lib

--- src/result.test.ts
+++ src/result.test.ts
@@ -1,23 +1,34 @@
 import { describe, it } from "node:test";
 import assert from "node:assert";
-import { notOk, ok } from "./result.js";
+import { Ok, Err, isErr, isOk, unwrap } from "./result.js";
 
 describe("ok", () => {
   it("returns true for ok value", () => {
-    assert.strictEqual(ok(12), true);
+    assert.strictEqual(isOk(Ok(12)), true);
   });
 
   it("returns false for not ok", () => {
-    assert.strictEqual(ok(new Error("OH NO")), false);
+    assert.strictEqual(isOk(Err(new Error("OH NO"))), false);
   });
 });
 
 describe("notOk", () => {
   it("returns true for notOk value", () => {
-    assert.strictEqual(notOk(new Error("OH NO")), true);
+    assert.strictEqual(isErr(Err(new Error("OH NO"))), true);
   });
 
   it("returns false for ok value", () => {
-    assert.strictEqual(notOk(12), false);
+    assert.strictEqual(isErr(Ok(12)), false);
+  });
+});
+
+describe("unwrap", () => {
+  it("returns value for Ok", () => {
+    assert.strictEqual(unwrap(Ok(12)), 12);
+  });
+
+  it("throws for Err", () => {
+    const err = new Error("Something broke");
+    assert.throws(() => unwrap(Err(err)));
   });
 });
