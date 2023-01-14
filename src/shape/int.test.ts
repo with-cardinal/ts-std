7f12e1323f1451a4c14857a9353be6349c897c72
Alan Johnson <nalanj@fastmail.com>
2023-01-14 17:37:48 -0500

    Get int handling moved over

--- src/shape/int.test.ts
+++ src/shape/int.test.ts
@@ -1,41 +1,38 @@
-// import { test } from "node:test";
-// import assert from "node:assert";
-// import { i32, ui32 } from "./int.js";
-// import { ShapeError } from "./validation-error.js";
+import { test } from "node:test";
+import assert from "node:assert";
+import { ui32, i32 } from "./int.js";
+import { Ok, unwrap } from "../result.js";
+import { ValidationError } from "./validation-error.js";
 
-// const i32Type = i32;
-// const ui32Type = ui32;
+test("i32 valid coerce", () => {
+  const res = i32(0);
+  assert.deepStrictEqual(res, Ok(0));
+});
 
-// test("i32 valid coerce", () => {
-//   const res = i32Type.coerce(0);
-//   assert.strictEqual(res, 0);
-// });
+test("i32 invalid coerce", () => {
+  assert.throws(() => unwrap(i32("test")), ValidationError);
+});
 
-// test("i32 invalid coerce", () => {
-//   assert.throws(() => i32Type.coerce("test"), ShapeError);
-// });
+test("i32 invalid coerce below min", () => {
+  assert.throws(() => unwrap(i32(-(2 ** 32))), ValidationError);
+});
 
-// test("i32 invalid coerce below min", () => {
-//   assert.throws(() => i32Type.coerce(-(2 ** 32)), ShapeError);
-// });
+test("i32 invalid coerce above max", () => {
+  assert.throws(() => unwrap(i32(2 ** 32)), ValidationError);
+});
 
-// test("i32 invalid coerce above max", () => {
-//   assert.throws(() => i32Type.coerce(2 ** 32), ShapeError);
-// });
+test("ui32 valid coerce", () => {
+  assert.deepStrictEqual(ui32(0), Ok(0));
+});
 
-// test("ui32 valid coerce", () => {
-//   const res = ui32Type.coerce(0);
-//   assert.strictEqual(res, 0);
-// });
+test("ui32 invalid coerce", () => {
+  assert.throws(() => unwrap(ui32("test")), ValidationError);
+});
 
-// test("ui32 invalid coerce", () => {
-//   assert.throws(() => ui32Type.coerce("test"), ShapeError);
-// });
+test("ui32 invalid coerce below min", () => {
+  assert.throws(() => unwrap(ui32(-1)), ValidationError);
+});
 
-// test("ui32 invalid coerce below min", () => {
-//   assert.throws(() => ui32Type.coerce(-1), ShapeError);
-// });
-
-// test("ui32 invalid coerce above max", () => {
-//   assert.throws(() => ui32Type.coerce(2 ** 32), ShapeError);
-// });
+test("ui32 invalid coerce above max", () => {
+  assert.throws(() => unwrap(ui32(2 ** 32)), ValidationError);
+});
--- src/shape/int.ts
+++ src/shape/int.ts
@@ -1,29 +1,31 @@
-// import { Shape, genericSafelyCoerce } from "./base.js";
-// import { error } from "./validation-error.js";
+import { Result, Ok, Err } from "../result.js";
+import type { Shape } from "./base.js";
+import { message, ValidationError } from "./validation-error.js";
 
-// function intCoerce(min: number, max: number) {
-//   return (val: unknown): number => {
-//     if (typeof val === "number") {
-//       if (val > max) {
-//         throw error(`must be an integer less than ${max}`);
-//       } else if (val < min) {
-//         throw error(`must be an integer greater than ${min}`);
-//       }
-//       return val;
-//     }
+function intCoerce(min: number, max: number) {
+  return (val: unknown): Result<number, ValidationError> => {
+    if (typeof val === "number") {
+      if (val > max) {
+        return Err(
+          new ValidationError([message(`must be an integer less than ${max}`)])
+        );
+      } else if (val < min) {
+        return Err(
+          new ValidationError([
+            message(`must be an integer greater than ${min}`),
+          ])
+        );
+      }
+      return Ok(val);
+    }
 
-//     throw error(`must be an integer between ${min} and ${max}`);
-//   };
-// }
+    return Err(
+      new ValidationError([
+        message(`must be an integer between ${min} and ${max}`),
+      ])
+    );
+  };
+}
 
-// const i32Coerce = intCoerce(-(2 ** 31) + 1, 2 ** 31 - 1);
-// export const i32: Shape<number> = {
-//   coerce: i32Coerce,
-//   safelyCoerce: genericSafelyCoerce(i32Coerce),
-// };
-
-// const ui32Coerce = intCoerce(0, 2 ** 32 - 1);
-// export const ui32 = {
-//   coerce: ui32Coerce,
-//   safelyCoerce: genericSafelyCoerce(ui32Coerce),
-// };
+export const i32: Shape<number> = intCoerce(-(2 ** 31) + 1, 2 ** 31 - 1);
+export const ui32: Shape<number> = intCoerce(0, 2 ** 32 - 1);
