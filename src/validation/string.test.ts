2ae6173a5bd6a821ed7023837de4c7088708aedd
Alan Johnson <nalanj@fastmail.com>
2023-01-17 18:19:30 -0500

    More string validation tweaks

--- src/validation/string.test.ts
+++ src/validation/string.test.ts
@@ -2,31 +2,31 @@ import assert from "node:assert";
 import test from "node:test";
 import { stringMax, stringMin } from "./string.js";
 
-test("min valid", () => {
+test("stringMin valid", () => {
   assert.deepStrictEqual(stringMin(5)("Testing"), []);
 });
 
-test("min invalid singular", () => {
+test("stringMin invalid singular", () => {
   assert.deepStrictEqual(stringMin(1)(""), ["must be longer than 1 character"]);
 });
 
-test("min invalid plural", () => {
+test("stringMin invalid plural", () => {
   assert.deepStrictEqual(stringMin(2)("a"), [
     "must be longer than 2 characters",
   ]);
 });
 
-test("max valid", () => {
+test("stringMax valid", () => {
   assert.deepStrictEqual(stringMax(5)("Test"), []);
 });
 
-test("max invalid singular", () => {
+test("stringMax invalid singular", () => {
   assert.deepStrictEqual(stringMax(1)("Test"), [
     "must be shorter than 1 character",
   ]);
 });
 
-test("max invalid plural", () => {
+test("stringMax invalid plural", () => {
   assert.deepStrictEqual(stringMax(2)("Test"), [
     "must be shorter than 2 characters",
   ]);
