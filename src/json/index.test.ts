be07b119cdd361e63cb27a0aac04177df748d484
Alan Johnson <nalanj@fastmail.com>
2023-01-20 22:03:08 -0500

    Add jsonible type
    Closes #27

--- src/json/index.test.ts
+++ src/json/index.test.ts
@@ -1,10 +1,12 @@
 import type { ExpectExtends, ExpectFalse } from "@type-challenges/utils";
-import type { ValidJSON } from ".";
+import type { JSONible, ValidJSON } from ".";
 
 export const anObject = { a: true, b: "text", c: { d: true }, e: [0, 1, 2] };
 export const aDate = new Date();
 
 export type checks = [
   ExpectExtends<ValidJSON, typeof anObject>,
-  ExpectFalse<ExpectExtends<ValidJSON, typeof aDate>>
+  ExpectFalse<ExpectExtends<ValidJSON, typeof aDate>>,
+  ExpectExtends<JSONible, typeof anObject>,
+  ExpectExtends<JSONible, typeof aDate>
 ];
