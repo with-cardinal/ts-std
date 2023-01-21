b5c724c4dcc2aec195f6019905c50ed94293c48d
Alan Johnson <nalanj@fastmail.com>
2023-01-21 10:29:41 -0500

    Update to use an idFn generator

--- src/id/index.test.ts
+++ src/id/index.test.ts
@@ -1,8 +1,13 @@
 import { test } from "node:test";
 import assert from "node:assert";
-import { genId } from "./index.js";
+import { idFn } from "./index.js";
 
-test("genId", () => {
-  const id = genId("a");
+test("idFn", () => {
+  const genId = idFn("a");
+  const id = genId();
+  const id2 = genId();
+
+  console.log(id, id2);
   assert(id.startsWith("a_"));
+  assert.strictEqual(id.slice(0, 6), id2.slice(0, 6));
 });
--- src/id/index.ts
+++ src/id/index.ts
@@ -1,10 +1,16 @@
 import { base58Encode } from "./base58.js";
 
-export function genId(prefix: string): string {
-  const buf = new Uint8Array(16);
+function genId(prefix: string): string {
+  const buf = new ArrayBuffer(16);
 
-  buf.fill(new Date().getTime(), 0, 5);
-  globalThis.crypto.getRandomValues(buf.subarray(5));
+  new DataView(buf).setUint32(0, new Date().getTime());
 
-  return `${prefix}_${base58Encode(buf)}`;
+  const uint8arr = new Uint8Array(buf);
+  globalThis.crypto.getRandomValues(uint8arr.subarray(5));
+
+  return `${prefix}_${base58Encode(uint8arr)}`;
+}
+
+export function idFn(prefix: string): () => string {
+  return () => genId(prefix);
 }
