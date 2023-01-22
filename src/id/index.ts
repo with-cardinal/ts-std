30dfd49e944cf63f4d05ab952a26003f316b9b54
Alan Johnson <nalanj@fastmail.com>
2023-01-22 07:57:38 -0500

    Update id gen to use 5 bytes from time, reduced to 1/10th of second instead of millisecond

--- src/id/index.ts
+++ src/id/index.ts
@@ -1,16 +1,15 @@
 import { base58Encode } from "./base58.js";
 
-const uint32max = 2 ** 32;
-
 function genId(prefix: string): string {
-  const buf = new ArrayBuffer(16);
+  const buf = new ArrayBuffer(19);
 
-  new DataView(buf).setUint32(0, new Date().getTime() % uint32max);
+  const time = Math.floor(Date.now() / 100);
+  new DataView(buf).setBigUint64(0, BigInt(time));
 
   const uint8arr = new Uint8Array(buf);
-  globalThis.crypto.getRandomValues(uint8arr.subarray(5));
+  globalThis.crypto.getRandomValues(uint8arr.subarray(8));
 
-  return `${prefix}_${base58Encode(uint8arr)}`;
+  return `${prefix}_${base58Encode(uint8arr.subarray(3))}`;
 }
 
 export function idFn(prefix: string): () => string {
